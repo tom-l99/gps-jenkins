b0cca847693132d4d7cb3257d2c47149b0fdc39e
Thomas Cooper 2018 (N0792395) <n0792395@ntu.ac.uk>
2020-03-01 18:11:33 +0000

    Started documenting test suite requirements

--- src/gpx-tests/steepestgradient-n0792395.cpp
+++ src/gpx-tests/steepestgradient-n0792395.cpp
@@ -5,7 +5,20 @@
 
 using namespace GPS;
 
-/* BOOST TEST SUITE DESCRIPTION */
+/* Route.steepestGradient() should return the largest change in gradient (in degrees)
+ * between to adjacent Positions. This change could be either a positive or negative
+ * but the function should be able to calculate the largest change regardless of its
+ * direction.
+ *
+ * However, this function requires there to be two Positions to compare against,
+ * therefore it should throw a "std::domain_error" exception when only one
+ * Position is given.
+ *
+ * This test suite starts by creating a test case that the function throws any
+ * "std::domain_errors" correctly. This is one by feeding it gpx data that only
+ * contains a single point.
+ *
+ * */
 
 BOOST_AUTO_TEST_SUITE( Route_steepestGradient_N0792395 )
 
@@ -23,8 +36,15 @@ BOOST_AUTO_TEST_CASE( single_route_point )
 //
 BOOST_AUTO_TEST_CASE( flat_route )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "ABCD.gpx", isFileName);
+   Route route = Route(LogFiles::GPXRoutesDir + "AFKPU.gpx", isFileName);
    BOOST_CHECK_EQUAL( route.numPositions(), 0.00 );
 }
+
+/*BOOST_AUTO_TEST_CASE( constant_gradient_route )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + NNumber + "UPS.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.numPositions(), );
+}
+*/
 
 BOOST_AUTO_TEST_SUITE_END()
