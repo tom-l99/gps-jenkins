195ea00e4ff0479a8b25014593367615d17db100
Sophie Clark 2018 (N0786783) <n0786783@ntu.ac.uk>
2020-03-06 18:21:35 +0000

    Created totalLength-n0786783.cpp

---
+++ src/gpx-tests/totalLength-n0786783.cpp
@@ -0,0 +1,14 @@
+#include <boost/test/unit_test.hpp>
+
+#include "route.h"
+#include "logs.h"
+
+using namespace GPS;
+
+
+BOOST_AUTO_TEST_SUITE(totalLength-n0786783)
+
+const bool isFileName = true; // All GPX data in this suite is loaded from files.
+const std::string defaultName = "Unnamed Route";
+
+BOOST_AUTO_TEST_SUITE_END()
