2a7d32db451c6dddc28a01cf20c5c7efca898085
Thomas Cooper 2018 (N0792395) <n0792395@ntu.ac.uk>
2020-03-06 16:55:41 +0000

    everything fixed back to normal

--- src/gpx-tests/maxElevation-n0792395.cpp
+++ src/gpx-tests/maxElevation-n0792395.cpp
@@ -1,7 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
-#include "logs.h"
 #include "route.h"
+#include "types.h"
 
 using namespace GPS;
 
@@ -9,29 +9,80 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE( Route_maxElevation_N0792395 )
 
-const bool isFileName = true; // All GPX data in this suite is loaded from files.
+const bool isFileName = false;
 
 const std::string NNumber = "/N0792395/"; //custom GPX data is loaded from this folder
 
 //
-BOOST_AUTO_TEST_CASE( single_route_point )
+BOOST_AUTO_TEST_CASE( elevation_absent )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
-   BOOST_CHECK_EQUAL( route.maxElevation(), -4000 );
+   const std::string gpxData =
+     "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"></rtept></rte></gpx>";
+   metres expectedElevation = 0;
+
+   Route route = Route(gpxData, isFileName);
+
+   BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation);
 }
 
 //
-BOOST_AUTO_TEST_CASE( flat_route )
+BOOST_AUTO_TEST_CASE( elevation_empty )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + "AFKPU.gpx", isFileName);
-   BOOST_CHECK_EQUAL( route.maxElevation(), 0.00 );
+   const std::string gpxData =
+     "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"><ele></ele></rtept></rte></gpx>";
+   metres expectedElevation = 0;
+
+   Route route = Route(gpxData, isFileName);
+
+   BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation );
 }
 
-/*BOOST_AUTO_TEST_CASE( constant_gradient_route )
+//
+BOOST_AUTO_TEST_CASE( natural_negative_elevation )
 {
-   Route route = Route(LogFiles::GPXRoutesDir + NNumber + "UPS.gpx", isFileName);
-   BOOST_CHECK_EQUAL( route.numPositions(), );
+    const std::string gpxData =
+      "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"><ele>-99999</ele></rtept></rte></gpx>";
+    metres expectedElevation = -424;
+
+    Route route = Route(gpxData, isFileName);
+
+    BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation );
 }
-*/
+
+//
+BOOST_AUTO_TEST_CASE( natural_positive_elevation )
+{
+    const std::string gpxData =
+      "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"><ele>-99999</ele></rtept></rte></gpx>";
+    metres expectedElevation = 8848;
+
+    Route route = Route(gpxData, isFileName);
+
+    BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation );
+}
+
+//
+BOOST_AUTO_TEST_CASE( extreme_negative_elevation )
+{
+    const std::string gpxData =
+      "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"><ele>-99999</ele></rtept></rte></gpx>";
+    metres expectedElevation = -99999;
+
+    Route route = Route(gpxData, isFileName);
+
+    BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation );
+}
+
+//
+BOOST_AUTO_TEST_CASE( extreme_positive_elevation )
+{
+    const std::string gpxData =
+      "<gpx><rte><name>Test</name><rtept lat=\"0\" lon=\"0\"><ele>99999</ele></rtept></rte></gpx>";
+    metres expectedElevation = 99999;
+
+    Route route = Route(gpxData, isFileName);
+
+    BOOST_CHECK_EQUAL( route.maxElevation(), expectedElevation );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
