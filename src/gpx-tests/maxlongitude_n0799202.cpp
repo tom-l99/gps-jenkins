4cff8d382618c7d35b75c313bde05c80e25f426a
Joshua Maccarthy 2018 (N0799202) <n0799202@ntu.ac.uk>
2020-03-10 13:01:49 +0000

    new maxlongitude test

--- src/gpx-tests/maxlongitude_n0799202.cpp
+++ src/gpx-tests/maxlongitude_n0799202.cpp
@@ -1,6 +1,37 @@
 #include "maxlongitude_n0799202.h"
+#include <boost/test/unit_test.hpp>
+
+#include "route.h"
+
+using namespace GPS;
+
+
+const bool isFileName = false;
 
 maxlongitude_n0799202::maxlongitude_n0799202()
 {
+
+}
+BOOST_AUTO_TEST_CASE(London_longitude_data)
+{
+    const std::string gpxData ="<gpx><rte><name>London</name><rtept lat=\"51.507351\" lon=\"-0.127758\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "-0.127758" );
+
+}
+
+BOOST_AUTO_TEST_CASE(Paris_longitude_data)
+{
+    const std::string gpxData ="<gpx><rte><name>Paris</name><rtept lat=\"48.856613\" lon=\"2.352222\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "2.352222" );
+
+}
+
+BOOST_AUTO_TEST_CASE(Tolhuin_longitude_data)
+{
+    const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"-67.190840\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "-67.190840" );
 
 }
