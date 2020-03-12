89d8dcd720006c5f9edbfe5a77a42f488e5103fd
Sophie Clark 2018 (N0786783) <n0786783@ntu.ac.uk>
2020-03-12 19:02:02 +0000

    Added some test cases to totalLength-n0786783.cpp

--- src/gpx-tests/totalLength-n0786783.cpp
+++ src/gpx-tests/totalLength-n0786783.cpp
@@ -5,10 +5,33 @@
 
 using namespace GPS;
 
+const bool isFileName = true; // All GPX data in this suite is loaded from files.
+const std::string defaultName = "Unnamed Route";
 
 BOOST_AUTO_TEST_SUITE(totalLength-n0786783)
 
-const bool isFileName = true; // All GPX data in this suite is loaded from files.
-const std::string defaultName = "Unnamed Route";
+
+BOOST_AUTO_TEST_CASE(Typical_Inputs_If_Zero)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(),0)
+}
+
+
+BOOST_AUTO_TEST_CASE(Negative_Inputs)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+
+    BOOST_CHECK(route.totalLength() > -1)
+
+}
+
+BOOST_AUTO_TEST_CASE(Typical_Inputs_Edge_Cases)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK(route.totalLength() > 0)
+
+
+}
 
 BOOST_AUTO_TEST_SUITE_END()
