dbe3887dec08b58156d2a3961593b0dd900b47da
Sophie Clark 2018 (N0786783) <n0786783@ntu.ac.uk>
2020-03-13 14:44:44 +0000

    Added some more test cases to totalLength-n0786783.cpp

--- src/gpx-tests/totalLength-n0786783.cpp
+++ src/gpx-tests/totalLength-n0786783.cpp
@@ -1,35 +1,79 @@
 #include <boost/test/unit_test.hpp>
-
+#include <limits>
 #include "route.h"
 #include "logs.h"
 
 using namespace GPS;
 
 const bool isFileName = true; // All GPX data in this suite is loaded from files.
-const std::string defaultName = "Unnamed Route";
 
 BOOST_AUTO_TEST_SUITE(totalLength-n0786783)
 
 
-BOOST_AUTO_TEST_CASE(Typical_Inputs_If_Zero)
+BOOST_AUTO_TEST_CASE(Typical_Inputs_Small_Input)
 {
     Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
-    BOOST_CHECK_EQUAL(route.totalLength(),0)
+    BOOST_CHECK(route.totalLength(), 0.8)
+
+
 }
 
 
-BOOST_AUTO_TEST_CASE(Negative_Inputs)
+BOOST_AUTO_TEST_CASE(Typical_Inputs_Three_Digit_Number)
 {
     Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK(route.totalLength(), 200)
 
-    BOOST_CHECK(route.totalLength() > -1)
 
 }
 
-BOOST_AUTO_TEST_CASE(Typical_Inputs_Edge_Cases)
+
+BOOST_AUTO_TEST_CASE(Typical_Inputs_Five_Digit_Number)
 {
     Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
-    BOOST_CHECK(route.totalLength() > 0)
+    BOOST_CHECK(route.totalLength(), 10500)
+
+
+}
+
+
+BOOST_AUTO_TEST_CASE(Negative_Inputs_Error_Case)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK(route.totalLength() < 0)
+
+    
+}
+
+
+BOOST_AUTO_TEST_CASE(Positive_Inputs_Error_Case)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK(route.totalLength() > std::numeric_limits<double>::max())
+
+    
+}
+
+
+BOOST_AUTO_TEST_CASE(Atypical_Inputs_Minimum_Edge_Case)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), 0)
+}
+
+
+BOOST_AUTO_TEST_CASE(Atypical_Input_Maximum_Edge_Case)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), std::numeric_limits<double>::max())
+
+}
+
+
+BOOST_AUTO_TEST_CASE(Typical_Input_Precision_Issue, * boost::unit_test::tolerance(0.001))
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), 100.001)
 
 
 }
