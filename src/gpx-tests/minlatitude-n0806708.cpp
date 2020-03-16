51c382d2fbdaa9944bf95aee15b87f26c8f74c37
Samuel Sutton 2018 (N0806708) <n0806708@ntu.ac.uk>
2020-03-16 15:07:30 +0000

    Added Test Case Names

--- src/gpx-tests/minlatitude-n0806708.cpp
+++ src/gpx-tests/minlatitude-n0806708.cpp
@@ -8,7 +8,67 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE (Route_minLatitude_N0806708)
 
-BOOST_AUTO_TEST_CASE (Example)
+
+
+BOOST_AUTO_TEST_CASE (onePosition)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE (manyPositions)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE (mixedLatitudePositions)
+{
+// negative and positive latitude numbers
+}
+
+BOOST_AUTO_TEST_CASE (duplicatePositions)
+{
+
+}
+
+//
+
+BOOST_AUTO_TEST_CASE (closePositions)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE (distantPositions)
+{
+
+}
+
+// error cases
+
+BOOST_AUTO_TEST_CASE (latitudeTooLow)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE (latitudeTooHigh)
+{
+
+}
+
+//
+
+BOOST_AUTO_TEST_CASE (latitudeBorderLow)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE (latitudeBorderHigh)
+{
+
+}
+
+//
+
+BOOST_AUTO_TEST_CASE (boostToleranceFLTPNT)
 {
 
 }
