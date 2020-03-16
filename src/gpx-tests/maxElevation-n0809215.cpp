5b56b9d856696cee5903c3b9037e90f55ce68efd
N0809215 <nathan.lewin2018@my.ntu.ac.uk>
2020-03-16 14:51:20 +0000

    Added source files

---
+++ src/gpx-tests/maxElevation-n0809215.cpp
@@ -0,0 +1,34 @@
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "types.h"
+#include "route.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE ( maxElevation-N0809215 )
+
+const bool isFileName = true;
+std::string gpxDat = LogFiles::GPXRoutesDir + "/N0809215/";
+
+/*BOOST_AUTO_TEST_CASE( singleValue )
+{
+    metres Result = 700;
+
+    Route routeData = Route(gpxDat + "singleValue.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL ( routeData.maxElevation(), Result);
+}
+*/
+
+BOOST_AUTO_TEST_CASE ( multipleElevation )
+{
+
+    metres Result = 573;
+
+    Route routeData = Route(gpxDat + "multipleElevation.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL ( routeData.maxElevation(), Result);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
