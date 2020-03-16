4b00ddf89ba372888779bc06c29e6a390b7c06c3
Sophie Clark 2018 (N0786783) <n0786783@ntu.ac.uk>
2020-03-16 19:18:37 +0000

    updated test suite and removed some useless empty test data files

--- src/gpx-tests/totalLength-n0786783.cpp
+++ src/gpx-tests/totalLength-n0786783.cpp
@@ -15,123 +15,151 @@ BOOST_AUTO_TEST_SUITE(totalLength-n0786783)
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_One_Digit_Number, * boost::unit_test::tolerance(0.001))
 {
-
+  //not created
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Two_Digit_Number,  * boost::unit_test::tolerance(0.001))
 {
-
+ //not created
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Three_Digit_Number,  * boost::unit_test::tolerance(0.001))
 {
 
-
+ //not created
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Five_Digit_Number,  * boost::unit_test::tolerance(0.001))
 {
-
+//not created
 
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Six_Digit_Number, * boost::unit_test::tolerance(0.001))
 {
-
+//not created
 
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Nine_Digit_Number, * boost::unit_test::tolerance(0.001))
 {
-
+//not created
 
 }
 
 BOOST_AUTO_TEST_CASE(Typical_Inputs_Twelve_Digit_Number, * boost::unit_test::tolerance(0.001))
 {
 
+//not created
+}
+
+BOOST_AUTO_TEST_CASE(Route_Is_Straight_Line, * boost::unit_test::tolerance(0.001))
+{
+   //use ABCDE.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Is_Straight_Line, , * boost::unit_test::tolerance(0.001))
+
+BOOST_AUTO_TEST_CASE(Route_Changes_Direction_Once,* boost::unit_test::tolerance(0.001))
+{
+   //use ABCDEJOYT.gpx
+
+}
+
+
+BOOST_AUTO_TEST_CASE(Route_Downward_Slope,* boost::unit_test::tolerance(0.001))
+{
+    //use MSY.gpx
+
+}
+
+
+BOOST_AUTO_TEST_CASE(Route_Upward_Slope,* boost::unit_test::tolerance(0.001))
 {
 
-
+    //use AGM.gpx
 }
 
 
-BOOST_AUTO_TEST_CASE(Route_Is_Changes_Direction_Once, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Looped,* boost::unit_test::tolerance(0.001))
 {
-
+    //use loop.gpx
 
 }
 
 
-BOOST_AUTO_TEST_CASE(Route_Goes_Every_Direction, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Spiral,* boost::unit_test::tolerance(0.001))
 {
-
+    //use spiral.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Crosses_Point_Already_Crossed, , * boost::unit_test::tolerance(0.001))
+
+BOOST_AUTO_TEST_CASE(Route_Goes_Back_On_Itself,* boost::unit_test::tolerance(0.001))
 {
-
+    //use ABCB.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Has_Single_Point, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Goes_Every_Direction, * boost::unit_test::tolerance(0.001))
 {
-
+    //use ABGLKF.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Has_Two_Points, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Has_Single_Point)
 {
-
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx" , isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), 0)
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Has_Three_Points, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Has_Two_Points, * boost::unit_test::tolerance(0.001))
 {
-
+//not created
 
 }
 
-
-BOOST_AUTO_TEST_CASE(Route_Has_Four_Points, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Has_Three_Points, * boost::unit_test::tolerance(0.001))
 {
-
+    //use AGM.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Has_Nine_Points, , * boost::unit_test::tolerance(0.001))
+
+BOOST_AUTO_TEST_CASE(Route_Has_Four_Points, * boost::unit_test::tolerance(0.001))
 {
-
+    //use ABCB.gpx
 
 }
 
-BOOST_AUTO_TEST_CASE(Route_Has_Two_Points_Same_Position, , * boost::unit_test::tolerance(0.001))
+BOOST_AUTO_TEST_CASE(Route_Has_Nine_Points, * boost::unit_test::tolerance(0.001))
 {
-
+    //use ABCDEJOYT.gpx
 
 }
 
-
-
-BOOST_AUTO_TEST_CASE(Atypical_Inputs_Minimum_Edge_Case)
+BOOST_AUTO_TEST_CASE(Route_Has_Every_Point, * boost::unit_test::tolerance(0.001))
 {
-    //Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
-    //BOOST_CHECK_EQUAL(route.totalLength(), 0)
+   //use spiral.gpx
+
 }
 
-/*
-BOOST_AUTO_TEST_CASE(Positive_Inputs_Error_Case)
+
+BOOST_AUTO_TEST_CASE(Route_Has_Two_Points_Same_Position)
 {
-    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
-    //BOOST_CHECK(route.totalLength() > maxTotalLength)
-
-    
+    Route route = Route(totalLengthTestDir + "TwoPointRouteWithSamePositions.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), 0)
 }
-*/
+
+
+
+BOOST_AUTO_TEST_CASE(No_Route_Edge_Case)
+{
+    Route route = Route(totalLengthTestDir + "emptyRoute.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalLength(), 0)
+}
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
