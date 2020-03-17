d8f5b01e1eaa4f76d8df334951d2a017ea6f1633
Joshua Maccarthy 2018 (N0799202) <n0799202@ntu.ac.uk>
2020-03-17 13:08:07 +0000

    finished boost test

--- src/gpx-tests/maxlongitude_n0799202.cpp
+++ src/gpx-tests/maxlongitude_n0799202.cpp
@@ -8,10 +8,12 @@ using namespace GPS;
 
 const bool isFileName = false;
 
+
 maxlongitude_n0799202::maxlongitude_n0799202()
 {
 
 }
+//Typical input of London's GPX data.
 BOOST_AUTO_TEST_CASE(London_longitude_data)
 {
     const std::string gpxData ="<gpx><rte><name>London</name><rtept lat=\"51.507351\" lon=\"-0.127758\"></rtept></rte></gpx>";
@@ -19,7 +21,7 @@ BOOST_AUTO_TEST_CASE(London_longitude_data)
     BOOST_CHECK_EQUAL( route.maxLongitude(), "-0.127758" );
 
 }
-
+//Typical input of Paris's GPX data.
 BOOST_AUTO_TEST_CASE(Paris_longitude_data)
 {
     const std::string gpxData ="<gpx><rte><name>Paris</name><rtept lat=\"48.856613\" lon=\"2.352222\"></rtept></rte></gpx>";
@@ -27,7 +29,7 @@ BOOST_AUTO_TEST_CASE(Paris_longitude_data)
     BOOST_CHECK_EQUAL( route.maxLongitude(), "2.352222" );
 
 }
-
+//Typical input of Tolhuin's GPX data
 BOOST_AUTO_TEST_CASE(Tolhuin_longitude_data)
 {
     const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"-67.190840\"></rtept></rte></gpx>";
@@ -35,24 +37,58 @@ BOOST_AUTO_TEST_CASE(Tolhuin_longitude_data)
     BOOST_CHECK_EQUAL( route.maxLongitude(), "-67.190840" );
 
 }
-
+//Test for maximum valid values.
 BOOST_AUTO_TEST_CASE(Possitve_edge_case)
 {
-    const std::string gpxData ="<gpx><rte><name>Pacific</name><rtept lat=\"-54.510570\" lon=\"179.999999\"></rtept></rte></gpx>";
+    const std::string gpxData ="<gpx><rte><name>Pacific</name><rtept lat=\"-54.510570\" lon=\"180.0\"></rtept></rte></gpx>";
     Route route = Route(gpxData, isFileName);
-    BOOST_CHECK_EQUAL( route.maxLongitude(), "179.999999" );
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "180.0" );
 }
 
+//Test for minimum valid value.
 BOOST_AUTO_TEST_CASE(Negative_edge_case)
 {
-    const std::string gpxData ="<gpx><rte><name>Pacific</name><rtept lat=\"-54.510570\" lon=\"-179.999999\"></rtept></rte></gpx>";
+    const std::string gpxData ="<gpx><rte><name>Pacific</name><rtept lat=\"-54.510570\" lon=\"-180.0\"></rtept></rte></gpx>";
     Route route = Route(gpxData, isFileName);
-    BOOST_CHECK_EQUAL( route.maxLongitude(), "179.999999" );
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "-180.0" );
 }
 
+//Test for maximum valid decimal values.
 BOOST_AUTO_TEST_CASE(decimal_place_boundary_case)
 {
     const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"179.999999999999999\"></rtept></rte></gpx>";
     Route route = Route(gpxData, isFileName);
     BOOST_CHECK_EQUAL( route.maxLongitude(), "179.999999999999999" );
+}
+
+//Test for minimum valid decimal values.
+BOOST_AUTO_TEST_CASE(negative_decimal_place_boundary_case)
+{
+    const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"-179.999999999999999\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), "-179.999999999999999" );
+}
+
+//Tests values that exceed valid ranges.
+BOOST_AUTO_TEST_CASE(Above_Max_Value)
+{
+    const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"180.1\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), NULL );
+}
+
+//Tests values that exceed valid ranges.
+BOOST_AUTO_TEST_CASE(Below_Minimum_value)
+{
+    const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\"-180.1\"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), NULL );
+}
+
+//Test for empty value.
+BOOST_AUTO_TEST_CASE(No_Longitude_value)
+{
+    const std::string gpxData ="<gpx><rte><name>Tolhuin</name><rtept lat=\"-54.510570\" lon=\" \"></rtept></rte></gpx>";
+    Route route = Route(gpxData, isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), NULL);
 }
