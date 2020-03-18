54efacbc3f1cf4ef4c7f313353f920c1564467fd
Nathan Lewin 2018 (N0809215) <n0809215@ntu.ac.uk>
2020-03-18 18:07:01 +0000

    Added Changes

--- src/gpx-tests/maxElevation-n0809215.cpp
+++ src/gpx-tests/maxElevation-n0809215.cpp
@@ -17,28 +17,77 @@ BOOST_AUTO_TEST_CASE( singleValue )
 
     Route routeData = Route(gpxDat + "singleValue.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL ( routeData.maxElevation(), Result);
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result) ;
 }
 
 
-BOOST_AUTO_TEST_CASE ( extremePositve )
+BOOST_AUTO_TEST_CASE( multipleValues )
+{
+    metres Result = 700;
+
+    Route routeData = Route(gpxDat + "multipleValues.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result) ;
+}
+
+
+BOOST_AUTO_TEST_CASE( extremePositve )
 {
     metres Result = 99999;
 
     Route routeData = Route(gpxDat + "extremePositive.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL ( routeData.maxElevation(), Result);
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
 }
 
 
-BOOST_AUTO_TEST_CASE ( extremeNegative )
+BOOST_AUTO_TEST_CASE( extremeNegative )
 {
     metres Result = -99999;
 
     Route routeData = Route(gpxDat + "extremeNegative.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL ( routeData.maxElevation(), Result);
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
 }
 
+
+BOOST_AUTO_TEST_CASE( positiveNatExtreme )
+{
+    metres Result = 8848;
+
+    Route routeData = Route(gpxDat + "positiveNatExtreme.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
+}
+
+
+BOOST_AUTO_TEST_CASE( negativeNatExtreme )
+{
+    metres Result = -10916;
+
+    Route routeData = Route(gpxDat + "positiveNatExtreme.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
+}
+
+
+BOOST_AUTO_TEST_CASE( positiveElevationExtreme )
+{
+    metres Result = 1.79769e+308;
+
+    Route routeData = Route(gpxDat + "positiveElevationExtreme.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
+}
+
+
+BOOST_AUTO_TEST_CASE( negativeElevationExtreme )
+{
+    metres Result = -1.79769e+308;
+
+    Route routeData = Route(gpxDat + "negativeElevationExtreme.gpx", isFileName);
+
+    BOOST_CHECK_EQUAL( routeData.maxElevation(), Result );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
