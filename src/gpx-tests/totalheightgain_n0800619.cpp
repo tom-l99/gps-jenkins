e2944bf9dba4710e6221cd2a5f9a9a8a236bbc75
Francesca Blidar <n0800619@my.ntu.ac.uk>
2020-03-19 19:15:59 +0000

    Added some tests: empty elevation field, blank elevation field, and some comments.

--- src/gpx-tests/totalheightgain_n0800619.cpp
+++ src/gpx-tests/totalheightgain_n0800619.cpp
@@ -7,36 +7,46 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE(totalHeightGain_N0800619)
 
-const metres defaulTotalHeightGain = 0;
+const metres defaulTotalHeightGain = 0.00000;
+const double percentageAccuracy = 0.00001;
 std::string gpxData = LogFiles::GPXRoutesDir + "/N0800619/";
 const bool isFileName = true; // All GPX data in this suite is provided from files.
 
-BOOST_AUTO_TEST_CASE(typicalInputs)
+//Typical values for elevation for which the total height gain changes value
+BOOST_AUTO_TEST_CASE(normalElevationValues)
 {
     const metres expectedValue = 0.00000;
-    const double percentageAccuracy = 0.00001;
     Route route = Route(gpxData + "ABCD.gpx", isFileName);
-    BOOST_CHECK_CLOSE( route.totalHeightGain(), 0.00000, percentageAccuracy );
+    BOOST_CHECK_CLOSE( route.totalHeightGain(), expectedValue, percentageAccuracy );
 }
 
-BOOST_AUTO_TEST_CASE(typicalInputs2)
+
+//Typical values for elevation for which the total height gain changes value
+BOOST_AUTO_TEST_CASE(normalElevationValues_SecondSet)
 {
-    const double percentageAccuracy = 0.00001;
+    const metres expectedValue = 20000.00000;
     Route route = Route(gpxData + "ABQWE.gpx", isFileName);
-    BOOST_CHECK_CLOSE( route.totalHeightGain(), 20000.00000, percentageAccuracy );
+    BOOST_CHECK_CLOSE( route.totalHeightGain(), expectedValue, percentageAccuracy );
 }
 
+//Single elevation value
 BOOST_AUTO_TEST_CASE(oneValue)
 {
     Route route = Route(gpxData + "Q.gpx", isFileName);
-    BOOST_CHECK_EQUAL( route.totalHeightGain() , defaulTotalHeightGain);
+    BOOST_CHECK_CLOSE( route.totalHeightGain(), defaulTotalHeightGain, percentageAccuracy);
 }
 
 BOOST_AUTO_TEST_CASE(valueJustAbovePercentageAccuracy)
 {
-    const double percentageAccuracy = 0.00001;
+    const metres expectedValue = 0.00010;
     Route route = Route(gpxData + "ABQ.gpx", isFileName);
-    BOOST_CHECK_CLOSE( route.totalHeightGain(), 0.00010, percentageAccuracy);
+    BOOST_CHECK_CLOSE( route.totalHeightGain(), expectedValue, percentageAccuracy);
+}
+
+BOOST_AUTO_TEST_CASE(negativeEleGivesNegDifferenceBetweenPoints)
+{
+    Route route = Route(gpxData + "negDiffSumZero.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(), defaulTotalHeightGain);
 }
 
 BOOST_AUTO_TEST_CASE(noElevationField)
@@ -45,21 +55,16 @@ BOOST_AUTO_TEST_CASE(noElevationField)
     BOOST_CHECK_THROW(route.totalHeightGain(), std :: invalid_argument);
 }
 
-//BOOST_AUTO_TEST_CASE(emptyElevationField)
-//{
-//    Route route = Route(gpxData + "noElevationValue.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(route.totalHeightGain(), defaulTotalHeightGain);
-//}
-
-BOOST_AUTO_TEST_CASE(negativeEleGivesNegDifferenceBetweenPoints)
+BOOST_AUTO_TEST_CASE(emptyElevationField)
 {
-    Route route = Route(gpxData + "negDiffSumZero.gpx", isFileName);
+    Route route = Route(gpxData + "noElevationValue.gpx", isFileName);
     BOOST_CHECK_EQUAL(route.totalHeightGain(), defaulTotalHeightGain);
 }
 
-BOOST_AUTO_TEST_CASE(wrongType)
+BOOST_AUTO_TEST_CASE(blankElevationField)
 {
-    BOOST_CHECK_EQUAL(1, 1);
+    Route route = Route(gpxData + "blankElevationField.gpx", isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(), defaulTotalHeightGain);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
