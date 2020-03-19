697dfb70f55956d92cebe394a469669ff57f7abe
Hussnain <hussnainaftab786@gmail.com>
2020-03-19 23:08:28 +0000

    Minor improvements/ bug removed

--- src/gpx-tests/maxlatitude-n0798917.cpp
+++ src/gpx-tests/maxlatitude-n0798917.cpp
@@ -8,7 +8,7 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE( maxLatitude )
 
-const bool isFileName = false;
+const bool isFileName = true;
 std::string Data = LogFiles::GPXRoutesDir + "/N0798917/";
 
 
@@ -16,9 +16,9 @@ std::string Data = LogFiles::GPXRoutesDir + "/N0798917/";
 BOOST_AUTO_TEST_CASE ( typical_route )
 {
     degrees maxLat = 0.179964;
-    Route route = Route(Data + "AHNT.gpx", true);
+    Route route = Route(Data + "AHNT.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL(Route.maxLatitude(), maxLat);
+    BOOST_CHECK_EQUAL(route.maxLatitude(), maxLat);
 }
 
 
@@ -26,18 +26,18 @@ BOOST_AUTO_TEST_CASE ( typical_route )
 BOOST_AUTO_TEST_CASE ( negative_latitude )
 {
     degrees maxLat = -0.089982;
-    Route route = Route(Data + "VR.gpx", true);
+    Route route = Route(Data + "VR.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL(Route.maxLatitude(), maxLat);
+    BOOST_CHECK_EQUAL(route.maxLatitude(), maxLat);
 }
 
 
 //Check that function works when Latitude = 0
 BOOST_AUTO_TEST_CASE ( latitude_zero )
 {
-    Route route = Route(Data + "VRN.gpx", true);
+    Route route = Route(Data + "VRN.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL(Route.maxLatitude(), 0);
+    BOOST_CHECK_EQUAL(route.maxLatitude(), 0);
 }
 
 
@@ -45,9 +45,9 @@ BOOST_AUTO_TEST_CASE ( latitude_zero )
 BOOST_AUTO_TEST_CASE ( max_valid )
 {
     degrees maxLat = 90;
-    Route route = Route(Data + "positive_edge.gpx", true);
+    Route route = Route(Data + "positive_edge.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL (Route.maxLatitude(), maxLat);
+    BOOST_CHECK_EQUAL (route.maxLatitude(), maxLat);
 }
 
 
@@ -55,18 +55,18 @@ BOOST_AUTO_TEST_CASE ( max_valid )
 BOOST_AUTO_TEST_CASE ( min_valid )
 {
     degrees maxLat = -90;
-    Route route = Route(Data + "negative_edge.gpx", true);
+    Route route = Route(Data + "negative_edge.gpx", isFileName);
 
-    BOOST_CHECK_EQUAL (Route.maxLatitude(), maxLat);
+    BOOST_CHECK_EQUAL (route.maxLatitude(), maxLat);
 }
 
 
 //Function should throw an out of range error if latitude is beyond the range i.e outside -90 to 90
 BOOST_AUTO_TEST_CASE ( above_max_valid )
 {
-    Route route = Route(Data + "max_boundary", true);
+    Route route = Route(Data + "max_boundary", isFileName);
 
-    BOOST_CHECK_THROW (Route.maxLatitude(), std::out_of_range)
+    BOOST_CHECK_THROW (route.maxLatitude(), std::out_of_range);
 }
 
 
