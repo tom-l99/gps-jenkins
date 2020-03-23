69e0b8467f4ac5ce2313f73166399c390ca36dd2
Linux 1804 <n0806708@my.ntu.ac.uk>
2020-03-23 09:49:12 -0700

    Uncommented Tests

--- src/gpx-tests/minlatitude-n0806708.cpp
+++ src/gpx-tests/minlatitude-n0806708.cpp
@@ -18,86 +18,67 @@ BOOST_AUTO_TEST_CASE (onePosition)
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.0179964);
 }
 
-//BOOST_AUTO_TEST_CASE (zeroLatitude)
-//{
-//    Route testData = Route(directory + "zeroLatitude.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), 0);
-//}
+BOOST_AUTO_TEST_CASE (zeroLatitude)
+{
+    Route testData = Route(directory + "zeroLatitude.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), 0);
+}
 
-//BOOST_AUTO_TEST_CASE (typicalLatitudeInput)
-//{
-//    Route testData = Route(directory + "typicalLatitudeInput.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
-//}
+BOOST_AUTO_TEST_CASE (typicalLatitudeInput)
+{
+    Route testData = Route(directory + "typicalLatitudeInput.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
+}
 
-//BOOST_AUTO_TEST_CASE (manyPositions)
-//{
-//    Route testData = Route(directory + "manyPositions.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), -1.79964);
-//}
+BOOST_AUTO_TEST_CASE (manyPositions)
+{
+    Route testData = Route(directory + "manyPositions.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), -1.79964);
+}
 
-//BOOST_AUTO_TEST_CASE (duplicatePositions)
-//{
-//    Route testData = Route(directory + "duplicatePositions.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
-//}
+BOOST_AUTO_TEST_CASE (duplicatePositions)
+{
+    Route testData = Route(directory + "duplicatePositions.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
+}
 
-//BOOST_AUTO_TEST_CASE (negativeMinimumLatitude)
-//{
-//    Route testData = Route(directory + "negativeMinimumLatitude.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
-//}
+BOOST_AUTO_TEST_CASE (negativeMinimumLatitude)
+{
+    Route testData = Route(directory + "negativeMinimumLatitude.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
+}
 
-//BOOST_AUTO_TEST_CASE (positiveMinimumLatitude)
-//{
-//    Route testData = Route(directory + "positiveMinimumLatitude.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
-//}
+BOOST_AUTO_TEST_CASE (positiveMinimumLatitude)
+{
+    Route testData = Route(directory + "positiveMinimumLatitude.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
+}
 
-//BOOST_AUTO_TEST_CASE (sameLatitudeDifferentLongitude)
-//{
-//    Route testData = Route(directory + "sameLatitudeDifferentLongitude.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
-//}
+BOOST_AUTO_TEST_CASE (sameLatitudeDifferentLongitude)
+{
+    Route testData = Route(directory + "sameLatitudeDifferentLongitude.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
+}
 
-
-////BOOST_AUTO_TEST_CASE (closePositions)
-////{
-
-////}
-////BOOST_AUTO_TEST_CASE (distantPositions)
-////{
-
-////}
-
-
-//BOOST_AUTO_TEST_CASE (latitudeBorderLow)
-//{
-//    Route testData = Route(directory + "latitudeBorderLow.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), -90);
-//}
-
-//BOOST_AUTO_TEST_CASE (latitudeBorderHigh)
-//{
-//    Route testData = Route(directory + "latitudeBorderHigh.gpx", isFileName);
-//    BOOST_CHECK_EQUAL(testData.minLatitude(), 90);
-//}
-
-//BOOST_AUTO_TEST_CASE (latitudeTooLow)
-//{
-    //Route testData = Route(directory + "latitudeTooLow.gpx", isFileName);
-    //BOOST_CHECK_EQUAL(testData.minLatitude(), NULL);
-//}
-
-//BOOST_AUTO_TEST_CASE (latitudeTooHigh)
-//{
-  //  Route testData = Route(directory + "latitudeTooHigh.gpx", isFileName);
-   // BOOST_CHECK_EQUAL(testData.minLatitude(), NULL);
-//}
-
-//BOOST_AUTO_TEST_CASE (boostToleranceFLTPNT)
+//BOOST_AUTO_TEST_CASE (closePositions)
 //{
 
 //}
+//BOOST_AUTO_TEST_CASE (distantPositions)
+//{
+
+//}
+
+BOOST_AUTO_TEST_CASE (latitudeBorderLow)
+{
+    Route testData = Route(directory + "latitudeBorderLow.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), -90);
+}
+
+BOOST_AUTO_TEST_CASE (latitudeBorderHigh)
+{
+    Route testData = Route(directory + "latitudeBorderHigh.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testData.minLatitude(), 90);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
