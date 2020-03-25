a8a07b58a0fd9f0d4417075b8920129f877569dc
Linux 1804 <n0806708@my.ntu.ac.uk>
2020-03-25 09:23:02 -0700

    Boost tolerance added

--- src/gpx-tests/minlatitude-n0806708.cpp
+++ src/gpx-tests/minlatitude-n0806708.cpp
@@ -22,7 +22,7 @@ const bool isFileName = true;
 std::string directory = LogFiles::GPXRoutesDir + "/N0806708/";
 
 // Test to prove that the function will return the minimum latitiude if only one position is entered
-BOOST_AUTO_TEST_CASE (onePosition)
+BOOST_AUTO_TEST_CASE (onePosition, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "onePosition.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.0179964);
@@ -36,49 +36,49 @@ BOOST_AUTO_TEST_CASE (zeroLatitude)
 }
 
 // Test to prove that the function will return the minimum latitiude of the route even if there is duplicate values
-BOOST_AUTO_TEST_CASE (duplicatePositions)
+BOOST_AUTO_TEST_CASE (duplicatePositions, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "duplicatePositions.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
 }
 
 // Test to prove that the function will return the minimum latitiude even if it returns a negative value
-BOOST_AUTO_TEST_CASE (negativeMinimumLatitude)
+BOOST_AUTO_TEST_CASE (negativeMinimumLatitude, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "negativeMinimumLatitude.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
 }
 
 // Test to prove that the function will return the minimum latitiude even if it returns a positive value
-BOOST_AUTO_TEST_CASE (positiveMinimumLatitude)
+BOOST_AUTO_TEST_CASE (positiveMinimumLatitude, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "positiveMinimumLatitude.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
 }
 
 // Test to prove that the function will return the minimum latitiude of a typical route input
-BOOST_AUTO_TEST_CASE (typicalLatitudeInput)
+BOOST_AUTO_TEST_CASE (typicalLatitudeInput, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "typicalLatitudeInput.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), -0.89982);
 }
 
 // Test to prove that the function will return the minimum latitiude if a large dataset is entered
-BOOST_AUTO_TEST_CASE (manyPositions)
+BOOST_AUTO_TEST_CASE (manyPositions, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "manyPositions.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), -1.79964);
 }
 
 // Test to prove that the function will return the minimum latitiude even if the longitudes are different
-BOOST_AUTO_TEST_CASE (sameLatitudeDifferentLongitude)
+BOOST_AUTO_TEST_CASE (sameLatitudeDifferentLongitude, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "sameLatitudeDifferentLongitude.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.89982);
 }
 
 // Test to prove that the function will return the minimum latitiude to at least 6 decimal places
-BOOST_AUTO_TEST_CASE (closePositions)
+BOOST_AUTO_TEST_CASE (closePositions, * utf::tolerance(0.0001))
 {
     Route testData = Route(directory + "closePositions.gpx", isFileName);
     BOOST_CHECK_EQUAL(testData.minLatitude(), 0.898471);
