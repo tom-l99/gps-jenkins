69de8e8dd52e2d8ac4a50bfa94d00a35ce69dada
Andrew Sheldon <andrewsheldon@Andrews-MBP.lan>
2020-03-26 22:26:04 +0000

    Addded more tests to suite and log files needed

--- src/gpx-tests/maxlongitude_n0795864.cpp
+++ src/gpx-tests/maxlongitude_n0795864.cpp
@@ -9,13 +9,18 @@ BOOST_AUTO_TEST_SUITE(maxlongitudeN0795864)
 
 const bool isFileName = true;
 std::string directory = LogFiles::GPXRoutesDir + "/N0795864/";
-//accepted tests
+//Typical tests
 BOOST_AUTO_TEST_CASE(NTU_longitude_data)
 {
     Route route = Route(directory + "SinglePosNTU.gpx", isFileName);
     BOOST_CHECK_EQUAL( route.maxLongitude(), -1.04054 );
 }
-//boundaries
+BOOST_AUTO_TEST_CASE(MultipleTypicalLongs)
+{
+    Route route = Route(directory + "MiltipleTypicalLongs.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), 100);
+}
+//Atypical tests
 BOOST_AUTO_TEST_CASE(Min)
 {
     Route route = Route(directory + "SinglePosMin.gpx", isFileName);
@@ -27,7 +32,12 @@ BOOST_AUTO_TEST_CASE(Max)
     Route route = Route(directory + "SinglePosMax.gpx", isFileName);
     BOOST_CHECK_EQUAL( route.maxLongitude(), 180 );
 }
-//multiple positions
+BOOST_AUTO_TEST_CASE(MultipleMin)
+{
+    Route route = Route(directory + "MiltipleMin.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.maxLongitude(), 100);
+}
+//Multiple positions
 BOOST_AUTO_TEST_CASE(Last)
 {
     Route route = Route(directory + "MultiplePosLast.gpx", isFileName);
